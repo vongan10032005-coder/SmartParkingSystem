5e7a104a4e9449a7571d97cce344a6de14070784
vongan10032005-coder <vongan10032005@gmail.com>
2026-03-29 17:34:15 +0700

    feat: add Retrofit API service, DTOs and AuthInterceptor

--- app/src/main/java/com/nhom/smartparking/data/remote/dto/Dtos.kt
+++ app/src/main/java/com/nhom/smartparking/data/remote/dto/Dtos.kt
@@ -1,2 +1,53 @@
 package com.nhom.smartparking.data.remote.dto
 
+data class LoginRequest(
+    val email: String,
+    val password: String
+)
+
+data class LoginResponse(
+    val token: String,
+    val user: UserDto
+)
+
+data class UserDto(
+    val id: Int,
+    val name: String,
+    val email: String
+)
+
+data class VehicleEntryRequest(
+    val license_plate: String,
+    val entry_time: Long
+)
+
+data class VehicleExitRequest(
+    val exit_time: Long
+)
+
+data class VehicleRecordResponse(
+    val id: Long,
+    val license_plate: String,
+    val entry_time: Long,
+    val exit_time: Long?,
+    val fee: Double?
+)
+
+data class HistoryResponse(
+    val data: List<VehicleRecordResponse>,
+    val total: Int,
+    val current_page: Int,
+    val last_page: Int
+)
+
+data class SyncRequest(
+    val license_plate: String,
+    val entry_time: Long,
+    val exit_time: Long?,
+    val fee: Double?
+)
+
+data class SyncResponse(
+    val synced: Int,
+    val failed: Int
+)
--- app/src/main/java/com/nhom/smartparking/data/remote/interceptor/AuthInterceptor.kt
+++ app/src/main/java/com/nhom/smartparking/data/remote/interceptor/AuthInterceptor.kt
@@ -1,2 +1,30 @@
 package com.nhom.smartparking.data.remote.interceptor
 
+import okhttp3.Interceptor
+import okhttp3.Response
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class AuthInterceptor @Inject constructor() : Interceptor {
+
+    private var token: String = ""
+
+    fun setToken(newToken: String) {
+        token = newToken
+    }
+
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val request = if (token.isNotEmpty()) {
+            chain.request().newBuilder()
+                .addHeader("Authorization", "Bearer $token")
+                .addHeader("Accept", "application/json")
+                .build()
+        } else {
+            chain.request().newBuilder()
+                .addHeader("Accept", "application/json")
+                .build()
+        }
+        return chain.proceed(request)
+    }
+}
